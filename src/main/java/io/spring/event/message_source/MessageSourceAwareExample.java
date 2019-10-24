3d85c1acb684158aad9b3697f1833c2a7d55eb9d
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-10-24 10:16:30 +0300

    change locale

--- src/main/java/io/spring/event/message_source/MessageSourceAwareExample.java
+++ src/main/java/io/spring/event/message_source/MessageSourceAwareExample.java
@@ -6,9 +6,9 @@ import java.util.Locale;
 
 public class MessageSourceAwareExample {
     public static void main(String[] args) {
-//        Locale.setDefault(Locale.US);
+        Locale.setDefault(Locale.US);
         //uncomment next line to change the locale
-        Locale.setDefault(Locale.FRANCE);
+//        Locale.setDefault(Locale.FRANCE);
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(Config.class);
         MyBean bean = context.getBean(MyBean.class);
