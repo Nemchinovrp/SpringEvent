022e6cdcfb9901be114226c344e6679a56f0a769
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-11-12 08:38:25 +0300

    minor refactoring

--- src/main/java/io/spring/event/spring_resources/ResourceLoaderService.java
+++ src/main/java/io/spring/event/spring_resources/ResourceLoaderService.java
@@ -18,19 +18,16 @@ public class ResourceLoaderService implements ResourceLoaderAware {
         this.resourceLoader = resourceLoader;
     }
 
-    /*
+    /**
      * This method reads the resource from root path. The method getResource()
      * accepts string value. In "file:pom.xml", no path is given so it considers the
      * root as path.
+     *
      * @throws IOException
-     */
+     **/
     public void showResourceDataUsingRoot() throws IOException {
         Resource resource = resourceLoader.getResource("file:pom.xml");
-
-        InputStream in = resource.getInputStream();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStream(resource)));
         while (true) {
             String line = reader.readLine();
             if (line == null)
@@ -40,20 +37,17 @@ public class ResourceLoaderService implements ResourceLoaderAware {
         reader.close();
     }
 
-    /*
+    /**
      * This method reads the resource from file path. As we have given the absolute
      * path the ResourceLoader loads the file from that location.
      * Ensure to modify the path "file:d:/test.txt" according to your local storage
+     *
      * @throws IOException
      */
     public void showResourceDataUsingFilePath() throws IOException {
         String filePath = "C:\\Users\\r.nemchinov\\Projects\\Spring-Event\\src\\main\\java\\io\\spring\\event\\spring_resources\\text.txt";
         Resource resource = resourceLoader.getResource("file:" + filePath);
-
-        InputStream in = resource.getInputStream();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStream(resource)));
         while (true) {
             String line = reader.readLine();
             if (line == null)
@@ -63,19 +57,16 @@ public class ResourceLoaderService implements ResourceLoaderAware {
         reader.close();
     }
 
-    /*
+    /**
      * This method reads the resource from class path. Using "classpath"
      * ResourceLoader loads the file from the thread context having resources
      * folder.
+     *
      * @throws IOException
      */
     public void showResourceDataUsingClassPath() throws IOException {
         Resource resource = resourceLoader.getResource("classpath:test.txt");
-
-        InputStream in = resource.getInputStream();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStream(resource)));
         while (true) {
             String line = reader.readLine();
             if (line == null)
@@ -85,18 +76,15 @@ public class ResourceLoaderService implements ResourceLoaderAware {
         reader.close();
     }
 
-    /*
+    /**
      * This method reads the resource from URL. ResourceLoader loads file via URL such
      * as file, an HTTP target, an FTP target, etc.
+     *
      * @throws IOException
      */
     public void showResourceDataUsingURL() throws IOException {
         Resource resource = resourceLoader.getResource("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/sha1.js");
-
-        InputStream in = resource.getInputStream();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStream(resource)));
         while (true) {
             String line = reader.readLine();
             if (line == null)
@@ -104,5 +92,9 @@ public class ResourceLoaderService implements ResourceLoaderAware {
             System.out.println(line);
         }
         reader.close();
+    }
+
+    private InputStream getInputStream(Resource resource) throws IOException {
+        return resource.getInputStream();
     }
 }
